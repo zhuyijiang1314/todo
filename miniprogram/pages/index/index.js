8c3883571669e4e5e6863d0fad67170b8303bf2c
zhuyijiang <634588012@qq.com>
2019-11-18 19:03:35 +0800

    1
    123

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,25 +1,18 @@
 // miniprogram/pages/index/index.js
-const db=wx.cloud.database();
-const todos=db.collection("todos");
-
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    tasks:null
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    todos.get().then(res=>{
-      this.setData({
-        tasks:res.data 
-      })
-    })
+
   },
 
   /**
@@ -36,5 +29,38 @@ Page({
 
   },
 
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
 
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
 })
