71700c49dae97061ac26ee9bfd165a692ef557f1
zhuyijiang <634588012@qq.com>
2019-11-19 18:20:46 +0800

    3

---
+++ miniprogram/pages/todoInfo/todoInfo.js
@@ -0,0 +1,80 @@
+// pages/todoInfo/todoInfo.js
+const db =wx.cloud.database();
+const todos =db.collection("todos");
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    task:{}
+  },
+  pageData:{
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.pageData.id = options.id;
+    todos.doc(options.id).get().then(res =>{
+      this.setData({
+        task:res.data
+      })
+    })
+
+
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
